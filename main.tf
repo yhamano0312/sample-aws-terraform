c4d53cf17270b310bbbd5972982377e71dfdbea0
Yusuke Hamano <72236227+yhamano0312@users.noreply.github.com>
2024-08-29 14:38:50 +0900

    fix name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Handson_yusukehamano"
+    Name = "Handson_yusukehamano2"
     TED = "20240829_Handson"
   }
 }
